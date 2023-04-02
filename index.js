e3ca8f0d830d2694046c545e7eb9193feab0510d
JaRoD41 <laurent.trinco@gmail.com>
2023-04-02 17:01:30 +0200

    tests to delete tags

--- index.js
+++ index.js
@@ -21,39 +21,53 @@ function init() {
 	const displayRecipes = new ViewRecipes()
 	displayRecipes.displayRecipesList(recipesToShow)
 
+	// je crée une instance de ma vue pour pouvoir afficher/supprimer les tags de filtrage des recettes
+	const displayTags = new FilterTagView()
+
 	// Code pour ajouter les écouteurs d'événements que j'envoie dans le controleur
 	const searchInput = document.querySelector('#search-zone')
 	const keywordsToClick = document.querySelectorAll('.accordion-body ul li')
-	keywordsToClick.forEach((keyword) => {
-		keyword.addEventListener('click', (event) => {
-			const keywordToSearch = event.target.innerText
-			console.log(keywordToSearch)
-			// JE DOIS ENVOYER LE TEXTE DU TAG CLIQUÉ DANS LE CONTROLEUR POUR FILTRER LES RECETTES APRES AVOIR VERIFIÉ SI LE TAG CLIQUÉ EST UN INGREDIENT, UN APPAREIL OU UN USTENSILE
-			// je crée une instance de ma vue pour pouvoir afficher les tags de filtrage des recettes
-			const displayTags = new FilterTagView()
-			displayTags.add(keywordToSearch, "ustensil")
-		})
-	})
+	const tagClose = document.querySelectorAll('.tag svg')
 
 	searchInput.addEventListener('input', (event) => {
 		const searchText = event.target.value
-		let mainInputLength = searchText.length 
+		let mainInputLength = searchText.length
 
 		if (mainInputLength > 3) {
 			controller.mainSearch(searchText)
 			controller.ingredientSearch(searchText)
 			controller.applianceSearch(searchText)
 			controller.ustensilSearch(searchText)
-
-		}
-		else if (mainInputLength === 0) {
+		} else if (mainInputLength === 0) {
 			controller.resetSearch()
 		}
 	})
 
-	// Code pour ajouter les écouteurs d'événements que j'envoie dans le controleur pour afficher les tags
+	// Code pour ajouter les écouteurs d'événements que j'envoie dans le controleur pour afficher/supprimer les tags
 
+	// Écouteur d'événement pour ajouter un tag de filtre
+	keywordsToClick.forEach((keyword) => {
+		keyword.addEventListener('click', (event) => {
+			const keywordToSearch = event.target.innerText
+			console.log(keywordToSearch)
+			// JE DOIS ENVOYER LE TEXTE DU TAG CLIQUÉ DANS LE CONTROLEUR POUR FILTRER LES RECETTES APRES AVOIR VERIFIÉ SI LE TAG CLIQUÉ EST UN INGREDIENT, UN APPAREIL OU UN USTENSILE
 
+			// J'appelle ma méthode créée pour pouvoir afficher les tags
+			displayTags.add(keywordToSearch, 'ustensil')
+			console.log(tagClose)
+			// J'appelle ma méthode créée pour pouvoir supprimer les tags
+			// Écouteur d'événement pour supprimer un tag de filtre
+			tagClose.forEach((tag) => {
+				tag.addEventListener('click', (event) => {
+					const tagToDelete = event.target.parentElement
+					console.log(tagToDelete);
+				})
+			})
+		})
+	})
+
+	
+	
 }
 
 init()
